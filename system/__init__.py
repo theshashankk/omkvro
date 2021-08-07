a9eb7671c5432c55afdd50a4d869b84b8a5db840
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-08-07 15:48:34 +0530

    Create __init__.py

---
+++ system/__init__.py
@@ -0,0 +1,16 @@
+# \\ @Albertt_xD //
+# // @Albertt_xD \\
+from telethon import TelegramClient
+from decouple import config
+import logging
+import time
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+# Basics
+APP_ID = config("APP_ID", default=None, cast=int)
+API_HASH = config("API_HASH", default=None)
+BOT_TOKEN = config("BOT_TOKEN", default=None)
+
+xd = TelegramClient('xd', APP_ID, API_HASH).start(bot_token=BOT_TOKEN) 

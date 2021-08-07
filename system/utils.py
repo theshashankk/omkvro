2aee42bb73b223b42746653feea19ab251ae9f8f
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-08-07 21:08:47 +0530

    Update utils.py

--- system/utils.py
+++ system/utils.py
@@ -2,42 +2,9 @@
 # \\ @Albertt_xD //
 import sys
 import logging
-from telethon import events
-import functools
-import glob
-import inspect
 import importlib
 from pathlib import Path
-from system.config import Config
-from . import xd
 
-bothandler = Config.COMD_HNDLR
-def xd_cmd(add_cmd, is_args=False):
-    def cmd(func):
-        if is_args:
-            pattern = bothandler + add_cmd + "(?: |$)(.*)"
-        else:
-            pattern = bothandler + add_cmd + "$"
-        xd.add_event_handler(
-            func, events.NewMessage(incoming=True, pattern=pattern)
-        )
-
-    return cmd
-
-def shashank_only():
-    def decorator(func):
-        @functools.wraps(func)
-        async def wrapper(event):
-            moms = Config.OWNER_ID
-            if event.sender_id == moms:
-                await func(event)
-            else:
-                pass
-
-        return wrapper
-
-    return decorator
-  
 def load_plugins(plugin_name):
     path = Path(f"system/plugins/{plugin_name}.py")
     name = "system.plugins.{}".format(plugin_name)
@@ -45,4 +12,5 @@ def load_plugins(plugin_name):
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["system.plugins." + plugin_name
+    sys.modules["system.plugins." + plugin_name] = load
+    print("smex has Imported " + plugin_name)

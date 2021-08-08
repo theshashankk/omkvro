fd7b9e6e8bd8eda606ac030067eec87e29fa0a7b
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-08-08 12:29:13 +0530

    Update _sticker.py

--- system/plugins/_sticker.py
+++ system/plugins/_sticker.py
@@ -4,4 +4,4 @@ from .. import xd
 @xd.on(events.NewMessage(incoming=True, pattern="/infoo"))
 async def stick(event):
   lwda = await event.get_reply_message()
-  await xd.reply(event.chat_id, f"Your sticker id: `{lwda.file.id}`")
+  await event.reply(event.chat_id, f"Your sticker id: `{lwda.file.id}`")

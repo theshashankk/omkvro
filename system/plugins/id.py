c807d47d24c0c2e15c8ac2e4ffca29c088aa8bfa
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-08-08 11:26:14 +0530

    Update id.py

--- system/plugins/id.py
+++ system/plugins/id.py
@@ -6,22 +6,23 @@ from .. import xd
 async def _(event):
     if event.reply_to_msg_id:
         await event.get_input_chat()
-        omk = [[Button.inline('Close 🚧', data='omkcls')]]
         r_msg = await event.get_reply_message()
         if r_msg.media:
             bot_api_file_id = pack_bot_file_id(r_msg.media)
             await xd.send_message(
                 event.chat_id,
-                f"Current Chat ID: `{event.chat_id}`\nFrom User ID: `{r_msg.from_id}`\nBot API File ID: `{bot_api_file_id}`",
-                button=omk,
-                 ),
+                "Current Chat ID: `{}`\nFrom User ID: `{}`\nBot API File ID: `{}`".format(
+                    str(event.chat_id), str(r_msg.from_id), bot_api_file_id
+                ),
             )
         else:
             await xd.send_message(
                 event.chat_id,
-                f"Current Chat ID: `{event.chat_id}`\nFrom User ID: `{r_msg.from_id}`",
-                button=omk
+                "Current Chat ID: `{}`\nFrom User ID: `{}`".format(
+                    str(event.chat_id), str(r_msg.from_id)
                 ),
             )
     else:
-        await xd.send_message(event.chat_id, f"Current Chat ID: `{event.chat_id}`")
+        await xd.send_message(
+            event.chat_id, "Current Chat ID: `{}`".format(str(event.chat_id))
+        )

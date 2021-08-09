2110d3eff52fb71b25f30705e908251e3592b697
theshashankk <theshashankk@users.noreply.github.com>
2021-08-09 09:43:01 +0000

    pylint: auto fixes

--- system/plugins/start.py
+++ system/plugins/start.py
@@ -1,6 +1,6 @@
 from telethon import Button, events
 
-from .. import xd, OWNER_ID
+from .. import OWNER_ID, xd
 
 
 @xd.on(events.NewMessage(incoming=True, pattern="/start"))
@@ -13,8 +13,8 @@ async def strat(e):
         return await e.reply(e.chat_id, "Hemlo sur", buttons=but)
     else:
         await e.reply("CONTACT MY OWNER TU ACCESS ME", button=omk)
-    
-    
+
+
 """
 await xd.send_file(e.chat_id, file="CAADBQADAgMAAlLIEVTp_wdXuvZM8QI")
 

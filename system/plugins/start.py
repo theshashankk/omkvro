cab2902bd4b5d5c1c69c01a4163eb2e8f85734a3
theshashankk <theshashankk@users.noreply.github.com>
2021-08-09 12:34:44 +0000

    pylint: auto fixes

--- system/plugins/start.py
+++ system/plugins/start.py
@@ -1,5 +1,5 @@
 from telethon import Button, events
-from telethon.tl.custom import button
+
 from .. import OWNER_ID, xd
 
 
@@ -8,7 +8,12 @@ async def strat(e):
     but = [[Button.url("Creator 💜", "t.me/Albertt_xD")]]
     but += [[Button.inline("Utils", data="utttils")]]
     but += [[Button.url("Gay", f"tg://user?id={e.sender.id}")]]
-    omk = [[Button.url("🐼 Creator 🐼", "t.me/Albertt_xD"), Button.url("🐼 Close 🐼", data="cls")]]
+    omk = [
+        [
+            Button.url("🐼 Creator 🐼", "t.me/Albertt_xD"),
+            Button.url("🐼 Close 🐼", data="cls"),
+        ]
+    ]
     if e.sender_id == OWNER_ID:
         return await e.reply(e.chat_id, "Hemlo sur", buttons=but)
     else:

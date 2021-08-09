59c53a2ba5b5cd9931b286397f7696ed8fd85181
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-08-09 14:27:17 +0530

    Create gcast.py

---
+++ system/plugins/gcast.py
@@ -0,0 +1,14 @@
+from . import *
+
+@xd.on(events.NewMessage(incoming=True, pattern="/gcast")
+async def _(e):
+    if str(e.sender_id) in all_sudo():
+        msg = e.pattern_match.group(1)
+        for x in all_chats():
+            try:
+                await bot.send_message(int(x), msg)
+                await e.reply(f"🚧 Broadcast done in {len(x)} Chats")
+            except Exception as err:
+                await e.reply(f"🚧 Error {str(err)}")  
+    else:
+        await e.reply("**chla ja bsdk**")

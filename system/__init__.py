18f846f335704bc91de75e092de8a7fc0964865d
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-08-09 07:29:13 +0530

    Update __init__.py

--- system/__init__.py
+++ system/__init__.py
@@ -2,12 +2,74 @@
 # // @Albertt_xD \\
 from telethon import TelegramClient
 from decouple import config
+from redis import ConnectionError, ResponseError, StrictRedis
 import logging
 import time
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
+if not Config.APP_ID:
+  LOGS.info("SED VAI PLEASE ADD API ID IN VARS")
+  exit(1)
 
+if not Config.API_HASH:
+  LOGS.info("SEE VRO PLEASE ADD API HASH IN VARS")
+  exit(1)
+  
+if not Config.BOT_TOKEN:
+  LOGS.info("SED VRO PLEASE ADD BOT TOKEN")
+  
+if not Config.REDIS_URI:
+  LOGS.info("#
+
+def connect_redis():
+    yuvi = Config.REDIS_URI.split(":")
+    op = StrictRedis(
+        host=yuvi[0],
+        port=yuvi[1],
+        password=Config.REDIS_PASSWORD,
+        charset="utf-8",
+        decode_responses=True,
+    )
+    return op
+
+try:
+    db = connect_redis()
+    LOGS.info("Connecting Db")
+    time.sleep(6)
+except ConnectionError as ce:
+    LOGS.info(f"Error ~ {ce}")
+    exit(1)
+except ResponseError as re:
+    LOGS.info(f"Error ~ {re}")
+    exit(1)
+except Exception as ex:
+    LOGS.info(f"Error ~ {ex}")
+    exit(1)
+
+START_TIME = datetime.now()
+
+try:
+    db.ping()
+except BaseException:
+    ok = []
+    LOGS.info("Can't connect to Database.... Restarting....")
+    for x in range(1, 6):
+        db = connect_redis()
+        time.sleep(5)
+        try:
+            if db.ping():
+                ok.append("ok")
+                break
+        except BaseException:
+            LOGS.info(f"Database Connection Failed ...  Trying To Reconnect {x}/5 ..")
+    if not ok:
+        LOGS.info("Database Connection Failed.....")
+        exit()
+    else:
+        LOGS.info("Reconnected To Server Succesfully")
+
+LOGS.info("Succesfully Established Connection With DataBase.")
 # Basics
 APP_ID = config("APP_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)

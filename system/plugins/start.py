230fa9cc3453437bea97e69c70f201abdcfa9c61
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-08-09 18:31:35 +0530

    Create start.py

--- system/plugins/start.py
+++ system/plugins/start.py
@@ -28,37 +28,12 @@ a#wait xd.send_file(e.chat_id, file="CAADBQADAgMAAlLIEVTp_wdXuvZM8QI")
 
     await e.reply(e.chat_id, buttons=but)
 """
-# Callback
+# Callback 
 @xd.on(events.callbackquery.CallbackQuery(data="cls"))
 async def omk(e):
     await e.delete()
 
-
 @xd.on(events.callbackquery.CallbackQuery(data="uttils"))
 async def ut(e):
-    await e.edit(
-        e.chat_id,
-        "HERE IS MY UTILS MENU",
-        button=[
-            [
-                Button.inline("Add Keyboard", data="addk"),
-                Button.inline("Start Buttons", data="go"),
-            ],
-            [
-                Button.inline("Remove Keyboard", data="removek"),
-                Button.inline("List Poles", data="listp"),
-            ],
-            [
-                Button.inline("Add Sudo", data="adds"),
-                Button.inline("Remove Sudo", data="rems"),
-                Button.inline("List Sudo", data="lists"),
-            ],
-            [
-                Button.inline("Eval", data="eval"),
-                Button.inline("Broadcast", data="bcast"),
-            ],
-            [Button.inline("Translate", data="tr"), Button.inline("Get Id", data="id")],
-            [Button.inline("My Master", data="me")],
-            [Button.inline("Back", data="start")],
-        ],
-    )
+    sed = [[Button.inline("🐼 Comming soon", data="soon")]]
+    await e.edit(e.chat_id, "HERE IS MY UTILS MENU", button=sed)

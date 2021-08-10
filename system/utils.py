2f151a299518ba152e563c143ea5a70ec158ef2c
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-08-10 16:43:10 +0530

    Update utils.py

--- system/utils.py
+++ system/utils.py
@@ -14,4 +14,4 @@ def load_plugins(plugin_name):
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
     sys.modules["system.plugins." + plugin_name] = load
-    print("┏━┅┅┄┄⟞⟦⟧⟝┄┄┉┉━┓\nimported\n┗━┅┅┄┄⟞⟦⟧⟝┄┄┉┉━┛ ")
+    print("┏━┅┅┄┄⟞⟦⟧⟝┄┄┉┉━┓\nImporting plugins\n┗━┅┅┄┄⟞⟦⟧⟝┄┄┉┉━┛ ")

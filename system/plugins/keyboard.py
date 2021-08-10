2ea5d94af5b491074b112eb67ff0d143e6eefa9d
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-08-10 16:23:41 +0530

    Create keyboard.py

---
+++ system/plugins/keyboard.py
@@ -0,0 +1,12 @@
+from telethon import events, Button
+from .. import db, xd
+#\\
+@xD.on(events.NewMessage(incoming=True, pattern="/add"))
+async def lwda(event):
+  if event.sender_id == SMEX_ID:
+    try:
+      lwda = event.pattern_match.group(1)
+      text.append(lwda)
+      await event.reply(event.chat_id, "Done added\nWanna Add more 🤔🤔\nType /add <txt>")
+    else:
+      await event.reply(event.chat_id, "jana chkke")

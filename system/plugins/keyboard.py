11949ff23f095f2fa6d560a392c4608845623590
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-08-10 16:36:32 +0530

    Update keyboard.py

--- system/plugins/keyboard.py
+++ system/plugins/keyboard.py
@@ -2,11 +2,13 @@ from telethon import events, Button
 from .. import db, xd
 #\\
 @xD.on(events.NewMessage(incoming=True, pattern="/add"))
-async def lwda(event):
-  if event.sender_id == SMEX_ID:
-    try:
-      lwda = event.pattern_match.group(1)
-      text.append(lwda)
-      await event.reply(event.chat_id, "Done added\nWanna Add more 🤔🤔\nType /add <txt>")
-  else:
-    await event.reply(event.chat_id, "JANA LWDE")
+async def add(event):
+    if event.sender_id == SMEX_ID:
+        try:
+            lwda = event.pattern_match.group(1)
+            text.append(lwda)
+            await event.reply(event.chat_id, f"__Done__ Added\n**{lwda}**")
+        except Exception as x:
+            await event.reply(f"Error {str(x)}")
+    else:
+        await event.reply(event.chat_id, "__JANA XHKKE__")

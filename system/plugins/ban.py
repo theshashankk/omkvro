d7f0a44e357cfdcb4e147628368cb33a7a6d02c3
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-08-10 15:36:43 +0530

    Create ban.py

---
+++ system/plugins/ban.py
@@ -0,0 +1,32 @@
+from .. import xd
+from system.plugins.db.xD import db
+
+@xd.on(events.NewMessage(incoming=True, pattern="/ban"))
+async def banhammer(event):
+    x = await event.get_reply_message()
+    if x is None:
+        return await event.edit("Please reply to someone to ban him.")
+    target = int(udB.get(str(x.id)))
+    if not is_blacklisted(target):
+        blacklist_user(target)
+        await asst.send_message(event.chat_id, f"#BAN\nUser - {target}")
+        await asst.send_message(
+            target,
+            "`GoodBye! You have been banned.`\n**Further messages you send will not be forwarded.**",
+        )
+    else:
+        return await asst.send_message(event.chat_id, f"User is already banned!")
+
+
+@xd.on(events.NewMessage(incoming=True, pattern="/unban"))
+async def banhammer(event):
+    x = await event.get_reply_message()
+    if x is None:
+        return await event.edit("Please reply to someone to ban him.")
+    target = int(udB.get(str(x.id)))
+    if is_blacklisted(target):
+        rem_blacklist(target)
+        await asst.send_message(event.chat_id, f"#UNBAN\nUser - {target}")
+        await asst.send_message(target, "`Congrats! You have been unbanned.`")
+    else:
+        return await asst.send_message(event.chat_id, f"User was never banned!")

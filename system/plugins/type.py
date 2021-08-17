2ce133452d9a2e127112c6e02b89dddd467e447f
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-08-17 21:24:31 +0530

    Create type.py

---
+++ system/plugins/type.py
@@ -0,0 +1,10 @@
+from .. import xd
+from telethon import events
+
+@xd.on(events.NewMessage(incoming=True, pattern="/type"))
+async def lol(event):
+  lwda = event.pattern_match_group(1)
+  if event.sender_id in SMEX_ID:
+    await xd.send_message(event.chat_id, f"{lwda}")
+  else:
+    await event.reply(event.chat_id, "jana chkke")

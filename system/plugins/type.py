a8504cfbb80868c124f7756c4b61c16477893d83
theshashankk <theshashankk@users.noreply.github.com>
2021-08-17 15:54:55 +0000

    pylint: auto fixes

--- system/plugins/type.py
+++ system/plugins/type.py
@@ -1,10 +1,12 @@
-from .. import xd
 from telethon import events
+
+from .. import xd
+
 
 @xd.on(events.NewMessage(incoming=True, pattern="/type"))
 async def lol(event):
-  lwda = event.pattern_match_group(1)
-  if event.sender_id in SMEX_ID:
-    await xd.send_message(event.chat_id, f"{lwda}")
-  else:
-    await event.reply(event.chat_id, "jana chkke")
+    lwda = event.pattern_match_group(1)
+    if event.sender_id in SMEX_ID:
+        await xd.send_message(event.chat_id, f"{lwda}")
+    else:
+        await event.reply(event.chat_id, "jana chkke")

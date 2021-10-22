9266a99b1685ddeedc6f4410ac4ebe379ec7abb7
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-10-22 15:26:43 +0530

    Update ban.py

--- system/plugins/ban.py
+++ system/plugins/ban.py
@@ -8,14 +8,12 @@ async def banhammer(event):
     x = await event.get_reply_message()
     if x is None:
         return await event.edit("Please reply to someone to ban him.")
-    target = int(db.get(str(x.id)))
-    if not is_blacklisted(target):
-        blacklist_user(target)
-        await xd.send_message(event.chat_id, f"#BAN\nUser - {target}")
-        await xd.send_message(
-            target,
-            "`GoodBye! You have been banned.`\n**Further messages you send will not be forwarded.**",
-        )
+    target = int(db.get(str(x.id)
+    await xd.send_message(event.chat_id, f"#BAN\nUser - {target}")
+    await xd.send_message(
+        target,
+        "`GoodBye! You have been banned.`\n**Further messages you send will not be forwarded.**",
+    )
     else:
         return await xd.send_message(event.chat_id, f"User is already banned!")
 
